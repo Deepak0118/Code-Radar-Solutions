513b301c8223ea9643637c77c2a1dc735430961e
Deepak Sharma <shivaay87081@gmail.com>
2025-03-28 00:15:34 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,41 +1,39 @@
 #include<stdio.h>
 int  main(){
-    int n;
-    scanf("%d",&n);
+    int number;
+    scanf("%d",&number);
     int arr[n];
-    for(int h =0;h<n;h++){
+    for(int h =0;h<number;h++){
         scanf("%d",&arr[h]);
     }
     int num =0,k=0;
-    for(int i =0;i<n;i++){
-        for(int j =0;j<n;j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                num =0;
-            }
-            else if(arr[j] < arr[j+1]){
-                k++;
-            }
-        }
-    }
+    
+        for(int j =0;j<number;j++){
+		for(int k =0;k<number-1;k++){
+			if(arr[k] > arr[k+1]){
+				int temp = arr[k];
+				arr[k] = arr[k+1];
+				arr[k+1] = temp;
+			}
+		}
+	}
+    
     int count =0;
-    for(int y =1;y<n;y++){
+    for(int y =1;y<number;y++){
         if(arr[0] == arr[y]){
             count ++;
         }
     }
-    if(count == n-1){
+    if(count == number-1){
         printf("%d",-1);
     }
     if(num ==0){
-        printf("%d",arr[n-2]);
+        printf("%d",arr[number-2]);
     }
     if(n==1){
         printf("%d",-1);
     }
-    if(k==n-1){
+    if(k==number-1){
         printf("%d",arr[n-2]);
     }
 }
