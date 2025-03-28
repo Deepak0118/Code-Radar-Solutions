0da67a71dde4efaab18c2dfccec6beadaae91ed9
Deepak Sharma <shivaay87081@gmail.com>
2025-03-28 19:25:00 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,28 +1,38 @@
-int findUnsortedSubarray(int arr[],int  n){
-    int ar[10000];
-    // int size = sizeof((arr[n])/arr[0]);
-    for(int k =0 ; k<n;k++){
-        ar[k] = arr[k];
+findUnsortedSubarray(int arr,int n){
+    int arr2[n];
+    for(int j =0;j<n;j++){
+        arr2[j] = arr[j];
     }
-
-    for(int i = 0;i<n;i++){
-        if(i+1<n){
-            if(ar[i] > ar[i+1]){
-                int temp = ar[i]; 
-                ar[i] = ar[i+1];
-                ar[i+1] = temp;
+    for(int i =0;i<n;i++){
+        for(int j =0;j<n;j++){
+            if(j+1<n){
+            if(arr[j] > ar[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
+        }
+        }
     }
+    int start;
+    int last;
+    for(int k =0;k<n;k++){
+        if(arr2[k] != arr[k]){
+            start = k;
+            break;
+        }
     }
-
-int count =0;
-for(int h =0 ;h<n;h++){
-    if(ar[h] != arr[h])
-    count++;
-    // else if((ar[h] == ar[h]) && (ar[h+1] != arr[h+1])){
-    //     count ++;
-    // }
+    for(int m=n-1;m>=0;m--){
+        if(arr2[m] != arr[m]){
+            last = m;
+            break;
+        }
+    }
+    int finali[100];
+    int p=0;
+    for(int d =start;d<=last;d++){
+        finali[p] = arr2[d];
+        p++;
+    }
+    return finali;
 }
-return count ;
-}
-
