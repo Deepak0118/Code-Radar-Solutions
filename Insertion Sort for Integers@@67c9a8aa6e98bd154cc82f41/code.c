018b7c2bb348c784bc38e4201962c9a695c4a593
Deepak Sharma <shivaay87081@gmail.com>
2025-03-30 15:12:02 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -1,11 +1,13 @@
-insertionSort(int arr[],int n){
+int insertionSort(int arr[],int n){
     for(int i =0;i<n;i++){
         for(int k =0;k<n;k++){
+            if(k+1<n){
             if(arr[k] > arr[k+1]){
                 int temp = arr[k];
                 arr[k] = arr[k+1];
                 arr[k+1] = temp;
              }
+            }
         }
     }
 }
